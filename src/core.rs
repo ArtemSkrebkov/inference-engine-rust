95a78b9c64b45e030d6e78d937b28f5e9704fc75
ArtemSkrebkov <skrebkov.art@gmail.com>
2020-05-03 21:44:13 +0300

    Add Core contructor

--- src/core.rs
+++ src/core.rs
@@ -1,8 +1,24 @@
-pub struct Core;
+extern crate inference_engine_sys_rust as ffi;
+
+use std::mem;
+use std::ffi::CString;
+
+pub struct Core {
+    core: Box<*mut ffi::ie_core_t>,
+}
+
 
 impl Core {
     pub fn new() -> Core {
-        Core
+        unsafe {
+            let mut core = Box::<*mut ffi::ie_core_t>::new(mem::zeroed());
+            let config_file = CString::new("").unwrap();
+            let config_file_ptr = config_file.as_ptr();
+            let status = ffi::ie_core_create(config_file_ptr, &mut *core);
+            Core {
+                core: core,
+            }
+        }
     }
 }
 
