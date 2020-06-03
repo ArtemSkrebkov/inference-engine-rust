e1a5b927b60329f3c2e569a3044ae7a6bd8b5c14
ArtemSkrebkov <skrebkov.art@gmail.com>
2020-06-03 08:42:26 +0300

    Check that inference with set blob working

--- src/core.rs
+++ src/core.rs
@@ -409,4 +409,35 @@ mod tests {
 
         assert_eq!(input_from_get[[0, 0, 0, 0]], elem);
     }
+
+    fn get_correct_inference_result_with_set_blob_for_input() {
+        let core = Core::new();
+        let network = core.read_network("test_data/resnet-50.xml",
+                        "test_data/resnet-50.bin");
+        let executable_network = core.load_network(network, "CPU");
+        let infer_request: InferRequest = executable_network.create_infer_request();
+        let input_image = ndarray_image::open_image("test_data/cat.png",
+            ndarray_image::Colors::Bgr).unwrap();
+        // FIXME: move repacking into a helper
+        let dims = input_image.dim();
+        let width = dims.0;
+        let height = dims.1;
+        let channels = dims.2;
+        let mut input = ArrayD::<f32>::zeros(IxDyn(&[1, channels, height, width]));
+        for c in 0..channels {
+            for h in 0..height {
+                for w in 0..width {
+                    input[[0, c, h, w]] = input_image[[h, w, c]] as f32;
+                }
+            }
+        }
+
+        let elem = input[[0, 0, 0, 0]];
+        infer_request.set_blob("data", input);
+        infer_request.infer();
+        let output = infer_request.get_blob("prob");
+        // FIXME: rulinalg is used only for argmax
+        let argmax = rulinalg::utils::argmax(output.into_slice().unwrap());
+        assert_eq!(argmax.0, 283);
+    }
 }
