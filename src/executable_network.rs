fbcfbb54cf6b8a16c3cdff5ba2b2bec4c56d41bf
ArtemSkrebkov <skrebkov.art@gmail.com>
2020-06-09 17:40:27 +0300

    Add a unit test for executable network

--- src/executable_network.rs
+++ src/executable_network.rs
@@ -20,10 +20,27 @@ impl ExecutableNetwork {
             let mut ie_infer_request = Box::<*mut ffi::ie_infer_request_t>::new(mem::zeroed());
             let status = ffi::ie_exec_network_create_infer_request(*self.ie_executable_network,
                 &mut *ie_infer_request);
-            utils::check_status(status);
+            utils::check_status_with_error_message(status, "ExecutableNetwork object is invalid.");
             InferRequest{
                 ie_infer_request: ie_infer_request,
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "GENERAL_ERROR: ExecutableNetwork object is invalid.")]
+    fn create_infer_request_from_invalid_executable_network() {
+        let executable_network = ExecutableNetwork{
+            ie_executable_network: unsafe {
+                Box::<*mut ffi::ie_executable_network_t>::new(mem::zeroed())
+            },
+        };
+
+        let _infer_request = executable_network.create_infer_request();
     }
 }
